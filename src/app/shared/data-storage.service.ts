9b0184a17d64279f8501da3af9109cf2c72a0a63
nsz0306 <nszabi@codingsans.com>
2018-03-16 13:27:52 +0100

    map added to get

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http'
 import { RecipeService } from '../recipes/recipe.service';
 import { Recipe } from '../recipes/recipe.model';
+import 'rxjs/add/operator/map'
 
 @Injectable()
 export class DataStorageService {
@@ -16,9 +17,19 @@ export class DataStorageService {
 
     getRecipes() {
         this.http.get('https://angular4sample-59ab0.firebaseio.com/recipes.json')
-            .subscribe(
+            .map(
                 (response: Response) => {
-                    const recipes: Recipe[] = response.json();
+                    const recipes: Recipe[] = response.json()
+                    for (let recipe of recipes) {
+                        if (recipe['ingredients']) {
+                            recipe['ingredients'] = [];
+                        }
+                    }
+                    return recipes;
+                }
+            )
+            .subscribe(
+                (recipes: Recipe[]) => {
                     this.recipeService.setRecipes(recipes);
                 }
             )
